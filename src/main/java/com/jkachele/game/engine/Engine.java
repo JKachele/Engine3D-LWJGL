013f42dfe00fedd958a06fde958bccb9b951478d
Justin Kachele <justin@kachele.com>
2022-10-17 16:58:47 -0400

    Initial Commit

---
+++ src/main/java/com/jkachele/game/engine/Engine.java
@@ -0,0 +1,17 @@
+/******************************************
+ *Project-------Engine3D-LWJGL
+ *File----------Engine.java
+ *Author--------Justin Kachele
+ *Date----------10/17/2022
+ *License-------GNU GENERAL PUBLIC LICENSE
+ ******************************************/
+package com.jkachele.game.engine;
+
+public class Engine implements Runnable{
+
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/com/jkachele/game/engine/Window.java
@@ -0,0 +1,11 @@
+/******************************************
+ *Project-------Engine3D-LWJGL
+ *File----------Window.java
+ *Author--------Justin Kachele
+ *Date----------10/17/2022
+ *License-------GNU GENERAL PUBLIC LICENSE
+ ******************************************/
+package com.jkachele.game.engine;
+
+public class Window {
+}
